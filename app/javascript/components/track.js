260963781724244324dba2b332610a911404a0c4
Wardormeur <wardormeur@localhost.localdomain>
2019-10-18 15:46:56 +0100

    Use d3 instead of vis

--- app/javascript/components/track.js
+++ app/javascript/components/track.js
@@ -1,71 +1,106 @@
+// Ref: http://www.d3noob.org/2014/01/tree-diagrams-in-d3js_11.html
+// https://observablehq.com/@bumbeishvili/d3-v5-organization-chart<Paste>
+// https://bl.ocks.org/Coola85/51a4bf6b231370cb56a6b7b8da2ff7a7
 class Track {
   constructor() {
     window.addEventListener('load', () => {
-      const nodes = [];
-      const edges = [];
+      const margin = {top: 20, right: 120, bottom: 20, left: 120};
+      const width = 960 - margin.right - margin.left;
+      const height = window.innerHeight - margin.top - margin.bottom;
+       
+      this.config = {
+        img: {
+          width: 300,
+          height: 200
+        },
+        card: {
+          margin: 120,
+        }
+      };
+      this.tree = d3.tree()
+       .size([height, width]);
 
-      // create a network
-      var container = document.getElementById('mynetwork');
-      if (container) {
-        track.forEach((step) => {
-          // const template = new NodeTemplate(step)
-          nodes.push({ id: step.id, 
-            label: step.name,
-            step: step,
-            //image: template.toSVG(), 
-            // shape: 'image', 
-            image: 'https://placekitten.com/300/200',
-            shape: 'image',
-            scaling: {
-              min: 30, 
-              max: 50,
-            }, 
-            fixed: true, // Only for preview
-          });
-          if (step.parent_id) {
-            edges.push({ from: step.parent_id, to: step.id });
-          }
-        });
-        nodes[0].x = 1;
-        nodes[0].y = 2;
-        var data = {
-          nodes: nodes,
-          edges: edges
-        };
-        var options = {
-          layout: {
-            hierarchical: { 
-              direction: 'UD',
-            }
-          }
-        };
-        var network = new vis.Network(container, data, options);
-        network.on('click', (params) => {
-          const node = nodes.find(node => params.nodes[0] === node.id);
-          window.open(node.step.resource.url, '_blank');
-        });
-      }
+      this.svg = d3.select('body > .container')
+	   .append('svg')
+       .attr('width', width + margin.right + margin.left)
+       .attr('height', height + margin.top + margin.bottom)
+       .append('g')
+       .attr('transform', `translate(${margin.left},${margin.top})`);
+      this.root = this.transform(window.track); 
+      this.update(this.root);
     });
   }
-}
-class NodeTemplate {
-  constructor(step) {
-    this.step = step;
+  transform(track) {
+    return d3.stratify()
+		.id(d => d.id)
+		.parentId(d => d.parent_id)(track);
   }
-  toSVG() {
-     const template = `<svg xmlns="http://www.w3.org/2000/svg" width="390" height="65">
-          <foreignObject x="15" y="10" width="100%" height="100%">
-          <div xmlns="http://www.w3.org/1999/xhtml" >
-            <img src="https://placekitten.com/300/200"/>
-            <div class="content">
-              <h2>${this.step.name}</h2>
-            </div>
-          </div>
-          </foreignObject>
-          </svg>`;
+  update(source) {
+    const config = this.config;
+    // Compute the new tree layout.
+    var nodes = this.tree(source).descendants(),
+     links = source.links();
 
-      return `data:image/svg+xml;charset=utf-8, ${encodeURIComponent(template)}`;
+    // Normalize for fixed-depth.
+    nodes.forEach((d) => {
+      d.y = d.depth * (config.img.height + config.card.margin); 
+      // Might be an issue with multi-leaf
+      d.x = d.x - config.img.width; 
+    });
+
+    // Declare the nodes¦
+    var node = this.svg.selectAll('g.node')
+     .data(nodes, function(d) { return d.id || (d.id = ++i); });
+  
+	/*node.append('circle')
+      .attr('fill', d => d.children ? '#555' : '#999')
+      .attr('r', 2.5);*/
+
+    // Enter the nodes.
+    var nodeEnter = node.enter().append('a')
+     .attr('class', 'node')
+     .attr('xlink:href', d => d.data.resource.url)
+     .attr('transform', (d) => `translate(${d.x},${d.y})`);
+
+    nodeEnter.
+      append('svg:image')
+      .attr('width', config.img.width)
+      .attr('height', config.img.height)
+      .attr('xlink:href', `https://placekitten.com/${config.img.width}/${config.img.height}`)
+    /*append('circle')
+     .attr('r', 10)
+     .style('fill', '#fff');*/
+
+    nodeEnter.append('text')
+     /*.attr('x', function(d) { 
+      return d.children || d._children ? -13 : 13; })
+     .attr('text-anchor', function(d) { 
+      return d.children || d._children ? 'end' : 'start'; })*/
+     .attr('x', 100)
+     .attr('y', config.img.height)
+     .attr('dy', '1em')
+     .text((d) => d.data.name )
+     .style('fill-opacity', 1);
+
+    // Declare the links!
+    var link = this.svg.selectAll('path.link')
+     .data(links, (d) => d.target.id);
+
+    // Enter the links.
+    link.enter().insert('path', 'g')
+     .attr('class', 'link')
+     .attr('d', this.vlink.bind(config));
+
+  }
+  link(d) {
+    return `M${d.source.y},${d.source.x}C${(d.source.y + d.target.y) / 2},${d.source.x} ${(d.source.y + d.target.y) / 2},${d.target.x} ${d.target.y},${d.target.x}`;
+  }
+  vlink(d) {
+    const bottom = (this.img.height) + d.source.y;
+    const middle = (this.img.width/2) + d.source.x;
+    const center = (this.img.width + d.source.x + d.target.x) /2;
+    // return `M${d.source.x},${d.source.y}C${(d.source.x + d.target.x) / 2},${d.target.y} ${(d.source.x + d.target.x) / 2},${d.source.y} ${d.target.x},${d.target.y}`;
+    return `M${middle},${bottom}C${center},${d.target.y} ${center},${bottom} ${d.target.x + this.img.width/2},${d.target.y}`;
   }
 }
-
 module.export = new Track();
