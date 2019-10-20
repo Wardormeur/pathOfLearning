16911c83abfa43423831c697516436fe9ab76160
Wardormeur <wardormeur@localhost.localdomain>
2019-10-20 16:42:55 +0100

    Scrap all rpf projects <3

--- app/jobs/scrap_rpf_projects_job.rb
+++ app/jobs/scrap_rpf_projects_job.rb
@@ -3,11 +3,33 @@ class ScrapRpfProjectsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    index = 0 
+    page_nb = get_size
+    while index < page_nb do
+      url = URI("https://learning-admin.raspberrypi.org/api/v1/en/projects")
+      url.query = Rack::Utils.build_nested_query({
+		"page" => {
+		  "number" => index,
+          "size" => 30
+        }
+	  })
+      res = Net::HTTP.get_response(url)
+      tutorials = ActiveSupport::JSON.decode(res.body)
+      Resource.transaction do
+        tutorials["data"].each do |tutorial|
+          attr = tutorial["attributes"]
+          Resource.create :name => attr["content"]["title"], :type_of => "html", :url => "https://projects.raspberrypi.org/en/projects/#{attr["repositoryName"]}"
+        end
+      end
+      index += 1
+    end
+  end
+  def get_size()
     res = Net::HTTP.get_response(URI("https://learning-admin.raspberrypi.org/api/v1/en/projects"))
     tutorials = ActiveSupport::JSON.decode(res.body)
-    tutorials["data"].each do |tutorial|
-      attr = tutorial["attributes"]
-      Resource.create :name => attr["content"]["title"], :type_of => "html", :url => "https://projects.raspberrypi.org/en/projects/#{attr["repositoryName"]}"
-    end
+    # we don't use next because it means we can inspect the number of pages to scrap
+    last_url = URI::parse(tutorials['links']['last'])
+    query = Rack::Utils.parse_nested_query(last_url.query)
+    return query.dig("page", "number").to_i || 0
   end
 end
